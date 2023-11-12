e0bcd1c109c98b23ffc7b1a796f208e106ca2a30
NicKyeaa <tin.jurcevic@gmail.com>
2023-11-12 22:02:58 +0100

    Deleted static dated

--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -1,16 +1,10 @@
 import { Fieldset, TextInput, Button, Group } from '@mantine/core';
 import TodoTable from './TodoTable';
 
-const todos = [
-  { id: 1, text: 'Task number one', done: false },
-  { id: 2, text: 'Make a to do list app', done: false },
-];
-
 async function fetchTodos() {
-  const res = await fetch(
-    'https://mocki.io/v1/3ec7374f-d068-4cfa-8030-cc2d9d42a881',
-    { cache: 'no-store' }
-  );
+  const res = await fetch('http://localhost:3500/todos:', {
+    cache: 'no-store',
+  });
   const data = await res.json();
   return data;
 }

0d6c3df98c11b0e4e62f525c44e2ee5149ddb792
NicKyeaa <tin.jurcevic@gmail.com>
2023-11-20 00:45:27 +0100

    resolved error with useDisclosure

--- src/app/todos/editModal.tsx
+++ src/app/todos/editModal.tsx
@@ -2,8 +2,6 @@
 import { Modal, Button } from '@mantine/core';
 
 const EditModal = ({ isOpen, onClose }) => {
-  const [opened, { open, close }] = useDisclosure(false);
-
   return (
     <>
       <Modal opened={isOpen} onClose={onClose} title='Authentication' centered>

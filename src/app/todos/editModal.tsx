6ec9fe9a3d93663a76e9143b47700b4772ac3854
NicKyeaa <tin.jurcevic@gmail.com>
2023-11-20 01:12:37 +0100

    Added the form for edit

--- src/app/todos/editModal.tsx
+++ src/app/todos/editModal.tsx
@@ -10,10 +10,29 @@ const EditModal = ({ isOpen, onClose, toDo }) => {
     },
   });
 
+  const handleEditToDo = (values) => {};
+
   return (
     <>
       <Modal opened={isOpen} onClose={onClose} title='Edit ToDo' centered>
         <h1>{toDo.title}</h1>
+        <form
+          onSubmit={form.onSubmit((values) => {
+            handleEditToDo(values);
+          })}
+        >
+          <Group>
+            <TextInput
+              placeholder='Edit the task title'
+              {...form.getInputProps('title')}
+            />
+            <TextInput
+              placeholder='Edit the task description'
+              {...form.getInputProps('longDescription')}
+            />
+            <Button type='submit'>Submit</Button>
+          </Group>
+        </form>
       </Modal>
     </>
   );
